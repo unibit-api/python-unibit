32afb30d50a37ae04e364d019f63f0bae87aa2ca
liuzulin <31738836+liuzulin@users.noreply.github.com>
2019-12-20 16:13:43 -0500

    add news api v2, corporate api v2, crypto api v2

---
+++ unibit_api_v2/corporate.py
@@ -0,0 +1,29 @@
+from .unibit import UniBit as ub
+
+class Corporate(ub):
+
+	def getCorporateSplits(self, ticker, startDate, endDate, selectedFields = "all", datatype = 'json', size = None):
+
+		if isinstance(ticker, list):
+			temp = "";
+			for item in ticker:
+				temp = temp + item + ","
+			ticker = temp
+
+
+		endpoints = 'company/actions/splits'
+
+		return self.make_request(endpoints=endpoints, data={'tickers': ticker, 'startDate': startDate, 'endDate': endDate, 'datatype':datatype ,'size':size})
+
+	def getCorporateDividends(self, ticker, startDate, endDate, selectedFields = "all", datatype = 'json', size = None):
+
+		if isinstance(ticker, list):
+			temp = "";
+			for item in ticker:
+				temp = temp + item + ","
+			ticker = temp
+
+
+		endpoints = 'company/actions/dividends'
+
+		return self.make_request(endpoints=endpoints, data={'tickers': ticker, 'startDate': startDate, 'endDate': endDate, 'datatype':datatype ,'size':size})
---
+++ unibit_api_v2/crypto.py
@@ -0,0 +1,16 @@
+from .unibit import UniBit as ub
+
+class CryptoPrice(ub):
+
+	def getHistoricalCryptoPrice(self, ticker, startDate, endDate, size = None, selectedFields = "all", datatype = "json"):
+
+		if isinstance(ticker, list):
+			temp = "";
+			for item in ticker:
+				temp = temp + item + ","
+			ticker = temp
+
+
+		endpoints = 'crypto/historical'
+
+		return self.make_request(endpoints=endpoints, data={'tickers': ticker, 'startDate': startDate, 'endDate': endDate, 'datatype':datatype ,'size':size})
